ed54ace94855d3be8b6ef6221000a40dfe15122d
Kanwarpreet-Singh <singhkanwarpreeet@gmail.com>
2024-07-15 22:20:53 +0530

    aer

--- src/TITLE.jsx
+++ src/TITLE.jsx
@@ -1,12 +1,18 @@
-function TITLE(){
-	return(
-		<>
-		<h1>wdwedew</h1>
-		<p>This is description</p>
+ 
+import './TITLE.css';
 
-		</>
-		);
+function TITLE({ heading, price = 3433, feat = [] }) {
+    const list = feat.map((item) => <li>{item}</li>);
+    console.log(list)
+    return (
+        <div className="TITLE">
+            <h2>{heading}</h2>
+            <ul>{list}</ul>
+            <h3>{price}</h3>
+        </div>
+    );
 }
 
-
 export default TITLE;
+
+ 
--- src/TITLEGROUP.jsx
+++ src/TITLEGROUP.jsx
@@ -1,12 +1,13 @@
 import TITLE from './TITLE.jsx'
 
-function titleGroup (){
-	return (
-		<div className="title">
-		< TITLE/>
-< TITLE/>
-< TITLE/>
+function TITLEGROUP (){
+	let arr = ["pen",'pencil']
+    return (
+        <div>
+        < TITLE heading="pen" price={54646} feat={arr}/>
+< TITLE heading="pencil" price = {656565}/>
+< TITLE heading="pen" />
 </div>
-		);
+        );
 }
 export default TITLEGROUP;
